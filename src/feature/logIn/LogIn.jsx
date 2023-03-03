adad9f62dc76da96722c4b3a499101ea80478a60
VIVID89 <wskth0930@gmail.com>
2023-03-04 01:55:37 +0900

    feat: 뷰 작업 완성

--- src/feature/logIn/LogIn.jsx
+++ src/feature/logIn/LogIn.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Input } from '../../common/Input';
+import Button from '../../common/Button';
 
-function LogIn() {
-  return <div>LogIn</div>;
+export default function LogIn() {
+  return (
+    <>
+      <LogInTitle>로그인</LogInTitle>
+      <LogInContainer>
+        <LogInInputLayout>
+          <Input loginInput type="text" placeholder="아이디를 입력해주세요" />{' '}
+        </LogInInputLayout>
+        <LogInInputLayout>
+          <Input loginInput type="text" placeholder="비밀번호를 입력해주세요" />{' '}
+        </LogInInputLayout>
+        <LogInInputLayout />
+        <Button login>로그인</Button>
+        <Button signUp>회원가입</Button>
+      </LogInContainer>
+    </>
+  );
 }
 
-export default LogIn;
+const LogInTitle = styled.div`
+  width: 340px;
+  height: 20px;
+  font-size: 28px;
+  line-height: 35px;
+  font-weight: 500;
+  text-align: center;
+  margin: auto;
+  margin-top: 90px;
+  margin-bottom: 50px;
+`;
+
+const LogInContainer = styled.div`
+  width: 340px;
+  height: 295.95px;
+  margin: auto;
+`;
+
+const LogInInputLayout = styled.div`
+  width: 340px;
+  height: 60px;
+`;
--- src/feature/signUp/SignUp.jsx
+++ src/feature/signUp/SignUp.jsx
@@ -1,7 +1,117 @@
 import React from 'react';
+import styled from 'styled-components';
+import { InputLayout, Input, MiniBox } from '../../common/Input';
+import Button from '../../common/Button';
 
-function SignUp() {
-  return <div>SignUp</div>;
+export default function SignUp() {
+  return (
+    <>
+      <SignUpTitleLayout>회원가입</SignUpTitleLayout>
+      <SignUpNeedsBox>
+        <RedPoint>*</RedPoint> 필수입력사항
+      </SignUpNeedsBox>
+      <SingUpLayout>
+        <InputLayout>
+          <MiniBox>
+            아이디<RedPoint>*</RedPoint>
+          </MiniBox>
+          <Input singupInput type="text" placeholder="아이디를 입력해주세요" />
+          <SignUpSideBox>
+            <Button overlap>중복확인</Button>
+          </SignUpSideBox>
+        </InputLayout>
+        <InputLayout>
+          <MiniBox>
+            닉네임<RedPoint>*</RedPoint>
+          </MiniBox>
+          <Input
+            singupInput
+            type="text"
+            placeholder="비밀번호를 입력해주세요"
+          />
+          <SignUpSideBox>
+            <Button overlap>중복확인</Button>
+          </SignUpSideBox>
+        </InputLayout>
+        <InputLayout>
+          <MiniBox>
+            이메일<RedPoint>*</RedPoint>
+          </MiniBox>
+          <Input singupInput type="text" placeholder="닉네임을 입력해주세요" />
+          <SignUpSideBox>
+            <Button overlap>중복확인</Button>
+          </SignUpSideBox>
+        </InputLayout>
+        <InputLayout>
+          <MiniBox>
+            비밀번호<RedPoint>*</RedPoint>
+          </MiniBox>
+          <Input
+            singupInput
+            type="text"
+            placeholder="비밀번호를 입력해주세요"
+          />
+          <SignUpSideBox />
+        </InputLayout>
+        <InputLayout>
+          <MiniBox>
+            비밀번호확인<RedPoint>*</RedPoint>
+          </MiniBox>
+          <Input
+            singupInput
+            type="text"
+            placeholder="비밀번호를 한번 더 입력해주세요"
+          />
+          <SignUpSideBox />
+        </InputLayout>
+      </SingUpLayout>
+      <SignUpButtonLayout>
+        <Button addReview>가입하기</Button>
+      </SignUpButtonLayout>
+    </>
+  );
 }
 
-export default SignUp;
+const SignUpTitleLayout = styled.div`
+  width: 640px;
+  height: 35px;
+  font-size: 28px;
+  line-height: 35px;
+  font-weight: 500;
+  text-align: center;
+  margin: 50px auto 50px auto;
+`;
+
+const SignUpNeedsBox = styled.div`
+  width: 640px;
+  height: 28.6px;
+  padding-bottom: 10px;
+  border-bottom: 2px solid rgb(51, 51, 51);
+  font-size: 12px;
+  color: rgb(102, 102, 102);
+  line-height: 17px;
+  text-align: right;
+  margin: auto;
+`;
+
+const SingUpLayout = styled.div`
+  width: 640px;
+  height: 340px;
+  margin: auto;
+  margin-bottom: 30px;
+`;
+
+const RedPoint = styled.span`
+  color: rgb(238, 106, 123);
+`;
+
+const SignUpSideBox = styled.div`
+  width: 120px;
+  height: 48px;
+  margin-left: 8px;
+`;
+
+const SignUpButtonLayout = styled.div`
+  width: 240px;
+  margin: auto;
+`;
