294d9497f9158caf8ec81f030f714ffe99b0a0cd
VIVID89 <122258921+VIVID89@users.noreply.github.com>
2023-03-04 22:07:55 +0900

    feat: 로그인 기능 및 서버 통신 테스트

--- src/modules/api/api.js
+++ src/modules/api/api.js
@@ -1,5 +1,7 @@
 import { api } from './axiosbase';
 // import HandleToken from './HandelToken';
+import axios from 'axios';
+import Cookies from 'js-cookie';
 
 export const postRegister = async (data) => {
   const response = await api.post('api/users/signup', data);
@@ -23,3 +25,24 @@ export const postRegister = async (data) => {
 //     console.log(e);
 //   }
 // };
+
+export const postLogin = async (data) => {
+  console.log('data', data);
+
+  const response = await api.post('api/users/login', data);
+  console.log(response.data.data);
+
+  const token = response.data.token;
+
+  const userInfo = response.data.data;
+
+  //세션 스토리지에 유저 정보 저장
+  sessionStorage.setItem('userInfo', JSON.stringify(userInfo));
+  console.log(sessionStorage.getItem('userInfo'));
+
+  //토큰 헤더에 저장
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+  Cookies.set('token', token, { expires: 7 }); // 7일 동안 유효한 쿠키 저장
+  console.log('로그인 성공');
+};
