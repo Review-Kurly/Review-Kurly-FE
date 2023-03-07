ac2f34d20f15d8f13076dc70e2c9400ebc0a1a60
Seogun95 <ksj083895@gmail.com>
2023-03-08 07:27:56 +0900

    feat: 마이페이지 토큰 없을 시 접근 제한

--- src/pages/mypage/Mypage.jsx
+++ src/pages/mypage/Mypage.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import isLogin from '../../modules/util/isLogin';
+import AlertModal from '../logIn/components/AlertModal';
 
 function Mypage() {
-  return <div>Mypage</div>;
+  const navigate = useNavigate();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    if (isLogin() === false) {
+      setIsModalOpen(true);
+    }
+  }, []);
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+    navigate('/login');
+  };
+
+  return (
+    <>
+      {isLogin() === true && <div>마이페이지</div>}
+      {isModalOpen && (
+        <AlertModal onClose={handleModalClose} onClick={handleModalClose}>
+          로그인하셔야 본 서비스를 이용하실 수 있습니다.
+        </AlertModal>
+      )}
+    </>
+  );
 }
 
 export default Mypage;
