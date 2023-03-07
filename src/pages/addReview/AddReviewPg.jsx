0165c993fd5c8273f7660bdafa8260e2e39f3dc3
Seogun95 <ksj083895@gmail.com>
2023-03-07 23:46:58 +0900

    feat: 접근 제한 된 컴포넌트를 모달을 통해 리다이랙트

--- src/pages/addReview/AddReviewPg.jsx
+++ src/pages/addReview/AddReviewPg.jsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import isLogin from '../../modules/util/isLogin';
+import AlertModal from '../logIn/components/AlertModal';
 import AddReview from './components/AddReview';
 
 function AddReviewPg() {
-  return <AddReview />;
+  const navigate = useNavigate();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    if (isLogin() === false) {
+      setIsModalOpen(true);
+    }
+  }, []);
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+    navigate('/login');
+  };
+
+  return (
+    <>
+      {isLogin() === true && <AddReview />}
+      {isModalOpen && (
+        <AlertModal onClose={handleModalClose}>
+          <h2>로그인이 필요합니다</h2>
+          <p>이 기능을 이용하시려면 로그인이 필요합니다.</p>
+          <button onClick={handleModalClose}>확인</button>
+        </AlertModal>
+      )}
+    </>
+  );
 }
 
 export default AddReviewPg;
---
+++ src/pages/logIn/components/AlertModal.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import styled from 'styled-components';
+//npm install framer-motion
+import { motion, AnimatePresence } from 'framer-motion';
+import Button from '../../../components/Button';
+
+export default function AlertModal({ onClose, children }) {
+  const backdropVariants = {
+    visible: { opacity: 1 },
+    hidden: { opacity: 0 },
+  };
+
+  const modalVariants = {
+    visible: { opacity: 1, y: 0 },
+    hidden: { opacity: 0, y: '-100%' },
+  };
+
+  return (
+    <AnimatePresence>
+      <Backdrop
+        variants={backdropVariants}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
+        onClick={onClose}
+      >
+        <ModalContainer
+          variants={modalVariants}
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <ModalMsg>{children}</ModalMsg>
+        </ModalContainer>
+      </Backdrop>
+    </AnimatePresence>
+  );
+}
+
+const Backdrop = styled(motion.div)`
+  ${(props) => props.theme.FlexRow};
+  position: fixed;
+  z-index: 99999;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.4);
+  backdrop-filter: blur(2px);
+`;
+
+const ModalContainer = styled(motion.div)`
+  background-color: white;
+  margin: auto; /* 추가 */
+  border-radius: 1rem;
+  /* filter: drop-shadow(rgba(0, 0, 0, 0.8) 2px 2px 20px); */
+`;
+
+const ModalMsg = styled.div`
+  font-size: 16px;
+  padding: 40px 30px;
+  text-align: center;
+  letter-spacing: -0.5px;
+  white-space: pre-line;
+  line-height: 21px;
+  color: #333333;
+`;
+
+const CloseContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  border-bottom-left-radius: 6px;
+  border-bottom-right-radius: 6px;
+  border-top: 1px solid rgb(247, 247, 247);
+  height: 56px;
+  padding: 0px;
+  margin-top: 0px;
+`;
